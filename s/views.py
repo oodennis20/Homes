93e10e85f658381d6c1f940202fb6a8f836e49aa
clyde <oodennis20@gmail.com>
2020-01-22 05:20:40 +0300

    added view functiond for signup,home and logout request

--- s/views.py
+++ s/views.py
@@ -2,16 +2,47 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from s import models
 
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+from django.contrib.auth import logout
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from django.contrib import messages
 
-
-
-# class ListTodo(generics.ListCreateAPIView):
-#     queryset = models.Todo.objects.all()
-#     serializer_class = TodoSerializer
-
-
-# class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = models.Todo.objects.all()
-#     serializer_class = TodoSerializer
+# Create your views here.
 
 def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+
+        return redirect('home') 
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def home(request):
+    if request.user.is_authenticated:
+        if Join.objects.filter(user_id=request.user).exists():
+            hood = Hood.objects.get(pk=request.user.join.hood_id.id)
+            posts = Posts.objects.filter(hood=request.user.join.hood_id.id)
+            businesses = Business.objects.filter(hood=request.user.join.hood_id.id)
+
+            return render(request, 'hoods/hood.html', {"hood": hood, "businesses": businesses, "posts": posts})
+        else:
+            neighbourhoods = Hood.objects.all()
+            return render(request, 'index.html', {"neighbourhoods": neighbourhoods})
+    else:
+        neighbourhoods = Hood.objects.all()
+        return render(request, 'index.html', {"neighbourhoods": neighbourhoods})
